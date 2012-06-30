2b9813938bef2f3b481cd4c9375e133b81f0d6e3
Gabriel Gilder <gabriel@gabrielgilder.com>
2012-06-29 17:37:04 -0700

    Add text input traits and text field security
    - Keyboard type
    - Return key type
    - Capitalization style

--- lib/layouts/categories/forms.rb
+++ lib/layouts/categories/forms.rb
@@ -7,3 +7,40 @@ class UITextField
     rounded: UITextBorderStyleRoundedRect
   }
 end
+
+class UITextInputTraits
+  KEYBOARD_TYPES = {
+    default: UIKeyboardTypeDefault,
+    ascii: UIKeyboardTypeASCIICapable,
+    numbers: UIKeyboardTypeNumbersAndPunctuation,
+    url: UIKeyboardTypeURL,
+    number_pad: UIKeyboardTypeNumberPad,
+    phone_pad: UIKeyboardTypePhonePad,
+    name_phone_pad: UIKeyboardTypeNamePhonePad,
+    email: UIKeyboardTypeEmailAddress,
+    decimal_pad: UIKeyboardTypeDecimalPad,
+    twitter: UIKeyboardTypeTwitter
+  }
+
+  RETURN_KEY_TYPES = {
+    default: UIReturnKeyDefault,
+    go: UIReturnKeyGo,
+    google: UIReturnKeyGoogle,
+    join: UIReturnKeyJoin,
+    next: UIReturnKeyNext,
+    route: UIReturnKeyRoute,
+    search: UIReturnKeySearch,
+    send: UIReturnKeySend,
+    yahoo: UIReturnKeyYahoo,
+    done: UIReturnKeyDone,
+    emergency: UIReturnKeyEmergencyCall
+  }
+
+  CAPITALIZATION_STYLES = {
+    default: UITextAutocapitalizationTypeNone,
+    none: UITextAutocapitalizationTypeNone,
+    words: UITextAutocapitalizationTypeWords,
+    sentences: UITextAutocapitalizationTypeSentences,
+    all: UITextAutocapitalizationTypeAllCharacters
+  }
+end
